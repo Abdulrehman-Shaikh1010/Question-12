831dc1aead0a5740f12290a47db8183e7207dbe1
Abdulrehman <you@example.com>
2024-04-08 02:42:59 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+var friendsName = ["Moiz", "Yasir", "Abdullah", "Ali"];
+console.log("Hello ".concat(friendsName[0], ", How are you?"));
+console.log("Hello ".concat(friendsName[2], ", How are you?"));
+console.log("Hello ".concat(friendsName[3], ", How are you?"));
+console.log("Hello ".concat(friendsName[1], ", How are you?"));
---
+++ main.ts
@@ -0,0 +1,9 @@
+let friendsName: string [] = ["Moiz", "Yasir", "Abdullah", "Ali" ];  
+
+console.log(`Hello ${friendsName[0]}, How are you?`);
+
+console.log(`Hello ${friendsName[2]}, How are you?`);
+
+console.log(`Hello ${friendsName[3]}, How are you?`);
+
+console.log(`Hello ${friendsName[1]}, How are you?`);
